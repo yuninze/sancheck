681a9bc9cc695fcbe40abaae35e843abb9a35e7e
yuninze <gajeongsick@gmail.com>
2024-04-28 17:13:33 +0900

    #

--- server/routes/sieve.js
+++ server/routes/sieve.js
@@ -3,6 +3,7 @@ const express=require("express")
 const router=express.Router()
 
 const session={}
+const blockWords=["php","cgi","sql"]
 
 router.use("*",(req,res,next)=>{
 	const ip=req.socket.remoteAddress
@@ -10,9 +11,10 @@ router.use("*",(req,res,next)=>{
 	session.datetime=new Date().toISOString()
 	session.ip=ip.slice(ip.indexOf(":",2)+1)
 	session.ua=req.get("User-Agent")
+	session.method=req.method
 	session.url=req.originalUrl
-	
-	if (session.ua==false || req.url.endsWith("php") || req.url.endsWith("cgi") || req.url.includes("sql") || req.url.includes("hwp")) {
+
+	if (session.ua===false || session.method!=="GET" || blockWords.some((q)=>{session.url.endsWith(q)})) {
 		res.end()
 	}
 	
